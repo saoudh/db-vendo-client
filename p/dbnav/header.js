70f4cdb2b0e1aafecfdad0d52c151c9175b70081
Traines <git@traines.eu>
2025-01-11 23:38:00 +0000

    linting

--- p/dbnav/header.js
+++ p/dbnav/header.js
@@ -1,8 +1,8 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 const getHeaders = (contentType) => {
 	return {
-		'X-Correlation-ID': uuidv4()+'_'+uuidv4(),
+		'X-Correlation-ID': uuidv4() + '_' + uuidv4(),
 		'Accept': contentType,
 		'Content-Type': contentType,
 	};
