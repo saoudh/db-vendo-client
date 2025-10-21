c2216120c9333446554eb932f4e96152ee1a6275
Traines <git@traines.eu>
2025-10-21 20:44:16 +0000

    set db profile to old dbnav domain as an alternative, since the new domain might have more agressive blocking

--- p/db/index.js
+++ p/db/index.js
@@ -1,35 +1,35 @@
-import dbnavBase from '../dbnav/base.json' with { type: 'json' };
+import base from './base.json' with { type: 'json' };
 import {products} from '../../lib/products.js';
 
 // journeys()
 import {formatJourneysReq} from '../dbnav/journeys-req.js';
-const {journeysEndpoint} = dbnavBase;
+const {journeysEndpoint} = base;
 
 // refreshJourneys()
 import {formatRefreshJourneyReq} from '../dbnav/journeys-req.js';
-const {refreshJourneysEndpointTickets, refreshJourneysEndpointPolyline} = dbnavBase;
+const {refreshJourneysEndpointTickets, refreshJourneysEndpointPolyline} = base;
 
 // locations()
 import {formatLocationsReq} from '../dbnav/locations-req.js';
 import {formatLocationFilter} from '../dbnav/location-filter.js';
-const {locationsEndpoint} = dbnavBase;
+const {locationsEndpoint} = base;
 
 // stop()
 import {formatStopReq} from '../dbnav/stop-req.js';
 import {parseStop} from '../dbnav/parse-stop.js';
-const {stopEndpoint} = dbnavBase;
+const {stopEndpoint} = base;
 
 // nearby()
 import {formatNearbyReq} from '../dbnav/nearby-req.js';
-const {nearbyEndpoint} = dbnavBase;
+const {nearbyEndpoint} = base;
 
 // trip()
 import {formatTripReq} from './trip-req.js';
-const {tripEndpoint} = dbnavBase;
+const {tripEndpoint} = base;
 
 // arrivals(), departures()
 import {formatStationBoardReq} from '../dbnav/station-board-req.js';
-const {boardEndpoint} = dbnavBase;
+const {boardEndpoint} = base;
 
 const profile = {
 	locale: 'de-DE',
--- readme.md
+++ readme.md
@@ -37,7 +37,7 @@ Depending on the configured profile, db-vendo-client will use multiple different
 | adminCode/operator    | only for journeys | only for journeys | only operator | ✅ |
 | stopovers             | not in boards | not in boards | ✅ | ✅ |
 | assumed backend API stability | less stable | more stable | less stable | more stable |
-| quotas | 60 requests per minute (IPv4) | 60 requests per minute (IPv4) | aggressive blocking (IPv4/IPv6) | depends on API key |
+| quotas | 60 requests per minute (IPv4) | 60 requests per minute (IPv4), possibly aggressive blocking | aggressive blocking (IPv4/IPv6) | depends on API key |
 
 
 > [!IMPORTANT]
